910836fad96f1eaff470093aaa42785c5646128a
Dan <dan.beloscurnic@isa.utm.md>
2024-03-01 13:57:25 +0200

    Add logging

--- NPS_WebAPI/Program.cs
+++ NPS_WebAPI/Program.cs
@@ -1,4 +1,3 @@
-
 using Persistence;
 using Application;
 using Microsoft.Extensions.Configuration;
@@ -17,7 +16,19 @@ namespace NPS_WebAPI
 
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Установка конфигурации Serilog
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+              //  .WriteTo.File("C:\\dan\\IntelectSoft\\NotesWebAppLog.txt", rollingInterval: RollingInterval.Month)
+                .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Warning) 
+                .CreateLogger();
+
+            // Регистрация службы Serilog в качестве посредника логирования запросов
+            builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                .ReadFrom.Configuration(hostingContext.Configuration)
+                .WriteTo.Console());
+
+            // Добавление служб в контейнер
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -25,26 +36,22 @@ namespace NPS_WebAPI
             builder.Services.AddPersistence(builder.Configuration);
             builder.Services.AddScoped<DbIntializer>();
 
-
             var app = builder.Build();
-            // Initialize database
-            app.UseCustomExceptionHandler();
-      
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var dbContext = services.GetRequiredService<Apps_DbContext>();
-                DbIntializer.Initialize(dbContext);
-            }
 
-            // Configure the HTTP request pipeline.
+            // Инициализация базы данных
+            app.UseCustomExceptionHandler();
+
+            // Использование Swagger в режиме разработки
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            // Настройка конвейера обработки HTTP запросов
             app.UseHttpsRedirection();
             app.UseAuthorization();
+           // app.UseSerilogRequestLogging(); 
             app.MapControllers();
             app.Run();
         }
